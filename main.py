fbe9a6c3a84dcb5180661cb44977d05f4025cf8f
PitchBlackNights <pitchblacknights@proton.me>
2025-04-29 13:39:33 -0500

    Add a space between menu options and controls; fix future bugs when deleting boards

--- main.py
+++ main.py
@@ -7,31 +7,42 @@
 
 from board import Board
 from ui import UI
+from tools import get_int, clamp_int
 import files, math, time
 
 
-# for cycle in range(0, 33):
+# files.delete_path(files.save_dir)
+# for cycle in range(0, 31):
 #     tmp_board = Board()
 #     tmp_board.generate(cycle)
 #     files.save_board(tmp_board)
-# for cycle in range(33, 66):
+# for cycle in range(0, 31):
 #     tmp_board = Board()
 #     tmp_board.generate(cycle)
 #     tmp_board.gameify(Board.Difficulty.MEDIUM)
 #     files.save_board(tmp_board)
 
 
-def get_int(text: str) -> int:
-    """Get an integer from the user"""
-    while True:
-        try:
-            user_int: int = int(input(text))
-            return user_int
-        except ValueError:
-            print("That's not a number!")
-            time.sleep(1)
-            # Sets cursor 2 lines up, then erases to the end of the screen
-            print("\x1b[2F\x1b[0J", end="")
+# Load saved boards into separate lists for filled and game boards
+saved_boards: list[Board] = files.load_saved_boards()
+filled_boards: list[Board] = []
+game_boards: list[Board] = []
+
+boards_per_page: int = 10
+
+
+def update_board_lists() -> None:
+    """Updates the lists keeping track of all the saved boards"""
+    global saved_boards, filled_boards, game_boards
+    saved_boards = files.load_saved_boards()
+    filled_boards = []
+    game_boards = []
+
+    for board in saved_boards:
+        if board.type == Board.Type.FULL:
+            filled_boards.append(board)
+        elif board.type == Board.Type.GAME:
+            game_boards.append(board)
 
 
 def main_menu() -> None:
@@ -39,7 +50,7 @@ def main_menu() -> None:
     while True:
         main_menu_ui: UI = UI(
             title="Main Menu",
-            options=[("1", "View Boards"), ("2", "Generate Boards"), ("X", "Exit")],
+            options=[("1", "View Boards"), ("2", "Generate Boards\n"), ("X", "Exit")],
         )
         main_menu_ui.show()
         user_choice: str = main_menu_ui.get_choice()
@@ -64,7 +75,7 @@ def generate_boards() -> None:
             title="Generate Boards",
             options=[
                 ("1", "Filled Boards"),
-                ("2", "Game Boards"),
+                ("2", "Game Boards\n"),
                 ("B", "Back"),
                 ("X", "Exit"),
             ],
@@ -104,7 +115,7 @@ def generate_boards__filled_boards() -> None:
 
         options_ui: UI = UI(
             header=False,
-            options=[("1", "Save"), ("2", "Skip"), ("B", "Back"), ("X", "Exit")],
+            options=[("1", "Save"), ("2", "Skip\n"), ("B", "Back"), ("X", "Exit")],
         )
         options_ui.show(clr_screen=False)
         user_choice: str = options_ui.get_choice()
@@ -124,6 +135,9 @@ def generate_boards__filled_boards() -> None:
         # GENERATE FILLED BOARDS: Save
         elif user_choice == "1":
             files.save_board(board)
+            # Update the saved boards lists to avoid undefined behavior
+            update_board_lists()
+
             print("Saved board!")
             time.sleep(1)
 
@@ -131,7 +145,7 @@ def generate_boards__filled_boards() -> None:
 def generate_boards__game_boards() -> None:
     """Menu for generating game boards"""
     num_to_gen: int = get_int("Number of boards to generate: ")
-    print("What difficulty level?")
+    print("\nWhat difficulty level?")
     options_ui: UI = UI(
         header=False,
         options=[("1", "Easy"), ("2", "Medium"), ("3", "Hard")],
@@ -152,7 +166,7 @@ def generate_boards__game_boards() -> None:
 
         options_ui: UI = UI(
             header=False,
-            options=[("1", "Save"), ("2", "Skip"), ("B", "Back"), ("X", "Exit")],
+            options=[("1", "Save"), ("2", "Skip\n"), ("B", "Back"), ("X", "Exit")],
         )
         options_ui.show(clr_screen=False)
         user_choice: str = options_ui.get_choice()
@@ -172,36 +186,30 @@ def generate_boards__game_boards() -> None:
         # GENERATE GAME BOARDS: Save
         elif user_choice == "1":
             files.save_board(board)
+            # Update the saved boards lists to avoid undefined behavior
+            update_board_lists()
+
             print("Saved board!")
             time.sleep(1)
 
 
 def view_boards() -> None:
     """Menu for viewing saved boards"""
-    while True:
-        # Load saved boards into appropriate lists
-        saved_boards: list[Board] = files.load_saved_boards()
-        filled_boards: list[Board] = []
-        game_boards: list[Board] = []
-        for board in saved_boards:
-            if board.type == Board.Type.FULL:
-                filled_boards.append(board)
-            elif board.type == Board.Type.GAME:
-                game_boards.append(board)
+    # Update the saved boards lists to avoid undefined behavior
+    update_board_lists()
 
+    while True:
         view_boards_ui: UI = UI(
             title="View Boards",
             options=[
                 ("1", "Filled Boards"),
-                ("2", "Game Boards"),
+                ("2", "Game Boards\n"),
                 ("B", "Back"),
                 ("X", "Exit"),
             ],
         )
         view_boards_ui.show()
         user_choice: str = view_boards_ui.get_choice()
-
-        boards_per_page: int = 10
 
         # VIEW BOARDS: Exit
         if user_choice == "X":
@@ -213,19 +221,22 @@ def view_boards() -> None:
 
         # VIEW BOARDS: Filled Boards
         elif user_choice == "1":
-            view_boards__filled_boards(boards_per_page)
+            view_boards__filled_boards()
 
         # VIEW BOARDS: Filled Boards
         elif user_choice == "2":
-            view_boards__game_boards(boards_per_page)
+            view_boards__game_boards()
 
 
-def view_boards__filled_boards(boards_per_page: int) -> None:
+def view_boards__filled_boards() -> None:
     """Menu for viewing saved filled boards"""
     board_page: int = 0
-    pages: int = math.ceil(len(filled_boards) / boards_per_page)
 
     while True:
+        pages: int = math.ceil(len(filled_boards) / boards_per_page)
+        # Limit the range of `board_page` (0 <= X <= `pages`) to avoid undefined behavior
+        board_page: int = clamp_int(0, board_page, pages)
+
         ui_options: list[tuple[str, str]] = []
         page_range = range(
             board_page * boards_per_page,
@@ -235,11 +246,13 @@ def view_boards__filled_boards(boards_per_page: int) -> None:
         # Populate UI options with this page's boards
         for index in page_range:
             ui_options.append((str(index + 1), f"Board #{filled_boards[index].id}"))
+        # Append a newline to the last option. This separates them from the menu screen controls
+        ui_options[-1] = (ui_options[-1][0], ui_options[-1][1] + "\n")
+
         if board_page != 0:
             ui_options.append(("-", "Prev. Page"))
         if board_page != pages - 1:
             ui_options.append(("+", "Next Page"))
-
         ui_options.append(("B", "Back"))
         ui_options.append(("X", "Exit"))
 
@@ -268,20 +281,18 @@ def view_boards__filled_boards(boards_per_page: int) -> None:
 
         # VIEW FILLED BOARDS: BOARD NUM
         elif int(user_choice) - 1 in page_range:
-            show_board_ui: UI = UI(
-                title=f"Viewing Board #{filled_boards[int(user_choice) - 1].id}",
-            )
-            show_board_ui.show()
-            print(filled_boards[int(user_choice) - 1].format())
-            input("Press enter to continue...")
+            view_boards__show_board_ui(filled_boards[int(user_choice) - 1])
 
 
-def view_boards__game_boards(boards_per_page: int) -> None:
+def view_boards__game_boards() -> None:
     """Menu for viewing saved game boards"""
     board_page: int = 0
-    pages: int = math.ceil(len(game_boards) / boards_per_page)
 
     while True:
+        pages: int = math.ceil(len(filled_boards) / boards_per_page)
+        # Limit the range of `board_page` (0 <= X <= `pages`) to avoid undefined behavior
+        board_page: int = clamp_int(0, board_page, pages)
+
         ui_options: list[tuple[str, str]] = []
         page_range = range(
             board_page * boards_per_page,
@@ -296,11 +307,13 @@ def view_boards__game_boards(boards_per_page: int) -> None:
                     f"Board #{game_boards[index].id} (DIFF: {game_boards[index].difficulty})",
                 )
             )
+        # Append a newline to the last option. This separates them from the menu screen controls
+        ui_options[-1] = (ui_options[-1][0], ui_options[-1][1] + "\n")
+
         if board_page != 0:
             ui_options.append(("-", "Prev. Page"))
         if board_page != pages - 1:
             ui_options.append(("+", "Next Page"))
-
         ui_options.append(("B", "Back"))
         ui_options.append(("X", "Exit"))
 
@@ -329,24 +342,52 @@ def view_boards__game_boards(boards_per_page: int) -> None:
 
         # VIEW GAME BOARDS: BOARD NUM
         elif int(user_choice) - 1 in page_range:
-            show_board_ui: UI = UI(
-                title=f"Viewing Board #{game_boards[int(user_choice) - 1].id} (DIFF: {game_boards[int(user_choice) - 1].difficulty})",
+            view_boards__show_board_ui(
+                game_boards[int(user_choice) - 1], difficulty=True
             )
-            show_board_ui.show()
-            print(game_boards[int(user_choice) - 1].format())
-            input("Press enter to continue...")
 
 
-# Load saved boards into separate lists for filled and game boards
-saved_boards: list[Board] = files.load_saved_boards()
-filled_boards: list[Board] = []
-game_boards: list[Board] = []
-for board in saved_boards:
-    if board.type == Board.Type.FULL:
-        filled_boards.append(board)
-    elif board.type == Board.Type.GAME:
-        game_boards.append(board)
+def view_boards__show_board_ui(board: Board, difficulty: bool = False) -> None:
+    show_board_ui: UI = UI(
+        title=f"Viewing Board #{board.id}"
+        + (f" (DIFF: {board.difficulty})" if difficulty else "")
+    )
+    show_board_ui.show()
+    print(f"{board.format()}\n")
+
+    show_board_ui_options: UI = UI(
+        header=False,
+        options=[("D", "Delete"), ("B", "Back"), ("X", "Exit")],
+    )
+    show_board_ui_options.show(clr_screen=False)
+    user_choice: str = show_board_ui_options.get_choice()
+
+    # SHOW BOARD UI OPTIONS: Exit
+    if user_choice == "X":
+        quit()
+
+    # SHOW BOARD UI OPTIONS: Back
+    elif user_choice == "B":
+        return
+
+    # SHOW BOARD UI OPTIONS: Delete
+    elif user_choice == "D":
+        # Update the saved boards lists to avoid undefined behavior
+        update_board_lists()
+
+        print("Deleted board!")
+        time.sleep(1)
 
 
-# Start the main menu
-main_menu()
+def __main() -> None:
+    """Where the root logic is executed"""
+    # Update saved boards from disk
+    update_board_lists()
+
+    # Start the UI loop
+    main_menu()
+
+
+# Good practice to prevent code from running when imported... for whatever reason this was imported
+if __name__ == "__main__":
+    __main()
---
+++ tools.py
@@ -0,0 +1,19 @@
+import time
+
+
+def clamp_int(min_num: int, num: int, max_num: int) -> int:
+    """Limits the range of `num` to between `min_num` and `max_num`"""
+    return max(min(max_num, num), min_num)
+
+
+def get_int(text: str) -> int:
+    """Gets an integer from the user"""
+    while True:
+        try:
+            user_int: int = int(input(text))
+            return user_int
+        except ValueError:
+            print("That's not a number!")
+            time.sleep(1)
+            # Sets cursor 2 lines up, then erases to the end of the screen
+            print("\x1b[2F\x1b[0J", end="")
